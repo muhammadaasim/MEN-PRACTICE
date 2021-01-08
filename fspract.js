08b6c286dcbd51555c6d8c9a3feb4d2260005db9
tabarak5 <tacitim5@gmail.com>
2021-01-09 00:26:00 +0500

    file write function added

--- fspract.js
+++ fspract.js
@@ -1,4 +1,18 @@
 var fs=require('fs');
-fs.readdir('./',function (err,files){
-    err ? console.log("Error : ",err) : console.log("Files : ",files);
+// fs.readdir('./',function (err,files){
+//     err ? console.log("Error : ",err) : console.log("Files : ",files);
+// })
+function readFile(srcpath){
+    fs.readFile(srcpath,'utf8',function (err,data){
+        err? console.log("Read Err :",err) : console.log(data);
+    })
+}
+
+function writeFile(savepath,data){
+fs.writeFile(savepath,data,function(err){
+    err? console.log("Err : ",err) : console.log("Write Sucessful");
 })
+}
+readFile("./helloworld.js");
+writeFile("./asim.txt","Hello I am asim !");
+readFile("./asim.txt");
