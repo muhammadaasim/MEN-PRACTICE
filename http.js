f3dbb273d731eda046501a249dd3b6e8e278ff09
tabarak5 <tacitim5@gmail.com>
2021-01-09 02:05:41 +0500

    added http

---
+++ http.js
@@ -0,0 +1,8 @@
+var http=require('http');
+var courses=[{id:1,name:'JS'},{id:2,name:'Node'}]
+var users=['userA','userB'];
+
+var server=http.createServer(function (request,response){
+    console.log("Got request",request);
+    // if(request.url)
+}).listen(3001);
