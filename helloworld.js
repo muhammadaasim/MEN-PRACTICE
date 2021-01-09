be3a346f043a7fa1070a5220a673a1bd9ff1016b
tabarak5 <tacitim5@gmail.com>
2021-01-10 00:38:19 +0500

    added json return

--- helloworld.js
+++ helloworld.js
@@ -7,7 +7,12 @@ app.set('port',process.env.PORT || 3001);
 app.get('./public',express.static(__dirname+'/public'));
 
 app.get('/',function (req,res){
-    res.send('hello world!');
+    // res.send('hello world!');
+    res.sendFile(__dirname+'/public/index.html')
+})
+
+app.get('/users',function (req,res){
+   res.json([{id:1,name:"aasim",caste:'soomro'},{id:2,name:'mohsin',caste:'soomro'}]);
 })
 
 app.listen(3001,function(){
