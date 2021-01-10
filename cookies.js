7f66d9a73b486895a126e6f2c2aa76bc0b9b0977
tabarak5 <tacitim5@gmail.com>
2021-01-11 01:52:32 +0500

    Cookies Clear function added

--- cookies.js
+++ cookies.js
@@ -5,10 +5,14 @@ const cookieParser=require('cookie-parser');
 app.use(cookieParser());
 
 app.get('/',(req,res)=>{
-res.cookie('myFirst Cookie');
+res.cookie('myFirstCookie','sent_by_Aasim');
 res.send('Cookies...');
 });
 
+app.get('/clear',  (req,res)=>{
+res.clearCookie('myFirstCookie');
+res.send("Cookies Cleared");
+})
 
 app.listen(3001,()=>{
     console.log('Express server is started at localhost:3001')
