0feae2a7347d1617245b61af5a883125e6e1b4d1
tabarak5 <tacitim5@gmail.com>
2021-01-10 16:17:00 +0500

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -6,20 +6,37 @@ const app=express();
 app.set('port',process.env.PORT || 3001);
 
 
-app.use(bodyParser.json());
+app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({extended:false}))
 
+
+//Making public folder as static so that everyone can visit without restrictation
 app.get('./public',express.static(__dirname+'/public'));
+
+
+//POST
+
+app.post('/',(req,res)=>{
+    res.end(JSON.stringify(req.body))
+})
+
+
+
+//Get Request html file response
 
 app.get('/',function (req,res){
     // res.send('hello world!');
-    res.sendFile(__dirname+'/public/index.html')
-})
+    // res.sendFile(__dirname+'/public/index.html')
+    res.send(JSON.stringify(req.body));
+});
+
+
+//Get request with JSON Response
 
 app.get('/users',function (req,res){
    res.json([{id:1,name:"aasim",caste:'soomro'},{id:2,name:'mohsin',caste:'soomro'}]);
 })
 
 app.listen(3001,function(){
-    console.log(`Express started no http://localhost:3001`)
+    console.log(`Express started at http://localhost:3001`)
 });
