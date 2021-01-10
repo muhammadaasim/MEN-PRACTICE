b30c4222a143c303c34f298e4154650714e8bdc0
tabarak5 <tacitim5@gmail.com>
2021-01-11 01:26:09 +0500

    Create routing.js

---
+++ routing.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const app = express();
+
+
+const router1=express.Router();
+const router2=express.Router();
+
+
+
+router1.get('/',(req,res)=>res.send('API is live'));
+router1.get('/user',(req,res)=>res.send('user is live'));
+router1.get('/group',(req,res)=>res.send('Group is live'));
+
+router2.get('/chat',(req,res)=>res.send('chat is live'));
+router2.get('/dashboard',(req,res)=>res.send('Dashboard is live'));
+router2.get('/:username',(req,res)=>res.send(JSON.stringify(req.params)));
+
+app.use('/v1',router1);
+app.use('/v2',router2);
+
+app.listen(3000, function () {
+    console.log(`Express Server Started on: http://localhost:3000`);
+});
