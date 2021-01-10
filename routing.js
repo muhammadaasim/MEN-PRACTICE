c274dc5b448e94034b8a33c605f8877c98a4c425
tabarak5 <tacitim5@gmail.com>
2021-01-11 01:37:07 +0500

    Update routing.js

--- routing.js
+++ routing.js
@@ -1,11 +1,9 @@
 const express = require('express');
-
 const app = express();
-
 
 const router1=express.Router();
 const router2=express.Router();
-
+const router3=express.Router();
 
 
 router1.get('/',(req,res)=>res.send('API is live'));
@@ -14,10 +12,11 @@ router1.get('/group',(req,res)=>res.send('Group is live'));
 
 router2.get('/chat',(req,res)=>res.send('chat is live'));
 router2.get('/dashboard',(req,res)=>res.send('Dashboard is live'));
-router2.get('/:username',(req,res)=>res.send(JSON.stringify(req.params)));
+router3.get('/:username',(req,res)=>res.send(JSON.stringify(req.params)));
 
 app.use('/v1',router1);
 app.use('/v2',router2);
+app.use('/user',router3);
 
 app.listen(3000, function () {
     console.log(`Express Server Started on: http://localhost:3000`);
